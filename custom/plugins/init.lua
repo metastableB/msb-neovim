84d3c0533f97fd48581f7240c508d9abbb61b565
metastableB <donkdennis@gmail.com>
2022-04-19 08:50:55 -0700

    Added trouble and modified focus to go back to editi screen during preview

--- custom/plugins/init.lua
+++ custom/plugins/init.lua
@@ -90,9 +90,8 @@ local trouble = {
   requires = "kyazdani42/nvim-web-devicons",
   config = function()
     require("trouble").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      -- automatically preview the location of the diagnostic. <esc> to
+      -- close preview and go back to last window
     }
   end,
 }
--- custom/plugins/lspconfig.lua
+++ custom/plugins/lspconfig.lua
@@ -14,29 +14,33 @@ M.setup_lsp = function(attach, capabilities)
    end
 end
 
--- Update: We've moved to trouble instead (plugin)
+-- Update: We've moved to trouble but still like these previews
 -- Disable Virtual Text for LSP and show in message area instead
 -- FROM : https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#show-line-diagnostics-automatically-in-hover-window
--- function PrintDiagnostics(opts, bufnr, line_nr, client_id)
---   bufnr = bufnr or 0
---   line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
---   opts = opts or {['lnum'] = line_nr}
---
---   local line_diagnostics = vim.diagnostic.get(bufnr, opts)
---   if vim.tbl_isempty(line_diagnostics) then return end
---
---   local diagnostic_message = ""
---   for i, diagnostic in ipairs(line_diagnostics) do
---     diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
---     print(diagnostic_message)
---     if i ~= #line_diagnostics then
---       diagnostic_message = diagnostic_message .. "\n"
---     end
---   end
---   vim.api.nvim_echo({{diagnostic_message, "Normal"}}, false, {})
--- end
---
--- vim.cmd [[ autocmd CursorHold * lua PrintDiagnostics() ]]
+function PrintDiagnostics(opts, bufnr, line_nr, client_id)
+  bufnr = bufnr or 0
+  line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
+  opts = opts or {['lnum'] = line_nr}
+
+  local line_diagnostics = vim.diagnostic.get(bufnr, opts)
+  if vim.tbl_isempty(line_diagnostics) then return end
+
+  local diagnostic_message = ""
+  for i, diagnostic in ipairs(line_diagnostics) do
+    diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
+    print(diagnostic_message)
+    if i ~= #line_diagnostics then
+      diagnostic_message = diagnostic_message .. "\n"
+    end
+  end
+  local win_id = vim.api.nvim_get_current_win()
+  -- get curr window id
+  vim.api.nvim_echo({{diagnostic_message, "Normal"}}, false, {})
+  -- switch to curr window
+  vim.api.nvim_set_current_win(win_id)
+end
+
+vim.cmd [[ autocmd CursorHold * lua PrintDiagnostics() ]]
 
 -- Disable virtual text
 vim.diagnostic.config({
